4ca60785c4f6a78483d75d803ca20d35ddc7b63f
pabl0gustavo <104775978+pabl0gustavo@users.noreply.github.com>
2022-08-18 09:08:17 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var canvas, backgroundImage;
+
+var questions;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+
+}
+
+
+
+// PROFESSORA onde eu coloco o SDK eu coloquei onde parecia estar corretor a senhora poderia sinalizar para mim
